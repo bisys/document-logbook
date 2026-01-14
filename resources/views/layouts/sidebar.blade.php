18125e08a9ace31293ce1186f60e9854a1b515b9
bisys <bisrifarhanullah00@gmail.com>
2026-01-14 15:28:03 +0700

    admin template integration

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,11 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">Stisla</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+        </div>
+        @include('layouts.menu')
+    </aside>
+</div>
